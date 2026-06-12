05b024335ea960ef03abb69479c6c6d25208ccbb
Corp <none>
2026-06-12 16:35:25 +0500

    Title mapping for AGE in draft

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ import warnings
 warnings.filterwarnings('ignore')
 
 from sklearn.base import clone
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import log_loss
+from sklearn.metrics import f1_score
 from sklearn.model_selection import StratifiedGroupKFold
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder, QuantileTransformer
 
